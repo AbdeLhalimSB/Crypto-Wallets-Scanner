7500008990128ab2eaf2a0f183db9a5d140a24ac
AbdeLhalim Sahbani <sahbani.2001@gmail.com>
2023-07-15 21:39:58 +0100

    Add threads

--- Crypto_Wallets_Scanner/Main.cs
+++ Crypto_Wallets_Scanner/Main.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
-using HtmlAgilityPack;
-
 
 namespace Crypto_Wallets_Scanner
 {
     public partial class Main : Form
     {
+        private BackgroundWorker worker;
+
         public Main()
         {
             InitializeComponent();
+
+            // Initialize the BackgroundWorker
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
         }
+
         List<string> wallets = new List<string>();
+
         private void button1_MouseHover(object sender, EventArgs e)
         {
             start.ForeColor = Color.White;
@@ -27,25 +37,50 @@ namespace Crypto_Wallets_Scanner
 
         private void button1_Click(object sender, EventArgs e)
         {
-            wallets.Clear();
-            List<string> blockchains = new List<string>();
-            blockchains.Add("https://bscscan.com/address/");
-            blockchains.Add("https://etherscan.io/address/");
-            blockchains.Add("https://polygonscan.com/address/");
-            blockchains.Add("https://ftmscan.com/address/");
-            blockchains.Add("https://snowtrace.io/address/");
-            blockchains.Add("https://moonriver.moonscan.io/");
-            blockchains.Add("https://moonscan.io/address/");
-            blockchains.Add("https://cronoscan.com/address/");
-            Wallets_Scanner ws = new Wallets_Scanner();
-            foreach (string item in wallets_txt.Lines)
+            result_txt.Clear();
+            if (!worker.IsBusy)
             {
-                wallets.Add(item);
+                // Clear the result textbox before starting the background task
+                result_txt.Text = string.Empty;
+
+                // Start the background task
+                worker.RunWorkerAsync();
             }
-            label4.Text = wallets.Count.ToString();
-            for(int i = 0; i < wallets.Count; i++)
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Perform the background task here
+            List<string> blockchains = new List<string>
             {
-                for(int b=0;b < 7; b++)
+                "https://bscscan.com/address/",
+                "https://etherscan.io/address/",
+                "https://polygonscan.com/address/",
+                "https://ftmscan.com/address/",
+                "https://snowtrace.io/address/",
+                "https://moonriver.moonscan.io/",
+                "https://moonscan.io/address/",
+                "https://cronoscan.com/address/"
+            };
+
+            Wallets_Scanner ws = new Wallets_Scanner();
+
+            Invoke((Action)(() =>
+            {
+                wallets.Clear();
+                foreach (string item in wallets_txt.Lines)
+                {
+                    wallets.Add(item);
+                }
+                label4.Text = wallets.Count.ToString();
+            }));
+
+            int totalWallets = wallets.Count * blockchains.Count;
+            int progress = 0;
+
+            foreach (string wallet in wallets)
+            {
+                for (int b = 0; b < blockchains.Count; b++)
                 {
                     string blockch;
                     if (b == 0)
@@ -80,10 +115,26 @@ namespace Crypto_Wallets_Scanner
                     {
                         blockch = "Cronos";
                     }
-                    result_txt.Text += ws.GetWalletBallance(wallets[i], blockchains[b], blockch);
+
+                    string result = ws.GetWalletBallance(wallet, blockchains[b], blockch);
+
+                    // Report progress and result to the UI thread
+                    worker.ReportProgress((progress * 100) / totalWallets, result);
+
+                    progress++;
                 }
-                
             }
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Update the UI with the progress and result
+            result_txt.AppendText(e.UserState.ToString());
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // The background task is completed
         }
 
         private void start_MouseEnter(object sender, EventArgs e)
@@ -91,12 +142,9 @@ namespace Crypto_Wallets_Scanner
             start.ForeColor = Color.White;
         }
 
-       
         private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            
+            // Handle key press event if needed
         }
-
-
     }
 }
