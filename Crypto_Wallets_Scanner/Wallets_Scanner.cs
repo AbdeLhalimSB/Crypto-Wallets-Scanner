07b68e12138390d7f7bdf14ab7eb703bdfae8ad1
unknown <sahbani.2001@gmail.com>
2023-09-01 10:46:20 +0100

    Fix the bug of showing btc balance even if the wallet have no balance

--- Crypto_Wallets_Scanner/Wallets_Scanner.cs
+++ Crypto_Wallets_Scanner/Wallets_Scanner.cs
@@ -25,6 +25,29 @@ namespace Crypto_Wallets_Scanner
         {
             this.wallet_address = wallet_address;
         }
+
+        static bool ContainsOnlyZeroesAndSpecialChars(string input)
+        {
+            foreach (char c in input)
+            {
+                // Check if the character is not '0' and not a special character
+                if (c != '0' && !IsSpecialCharacter(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool IsSpecialCharacter(char c)
+        {
+            // Define the set of special characters you want to consider
+            string specialChars = "!@#$%^&*()_+[]{}|;':\",./<>?`~\\-";
+
+            // Check if the character is in the set of special characters
+            return specialChars.Contains(c);
+        }
+
         public string GetWalletBallance(string wallet,string blockchaine,string blockch,string id)
         {
             string result = "";
@@ -38,7 +61,17 @@ namespace Crypto_Wallets_Scanner
                 if (bnb != null)
                 {
                     string balance = bnb.InnerText.Trim();
-                    result = blockch + " Network, Wallet: " + wallet + " | Balance: " + balance.Replace(" ","").Replace("\n","").Replace("{{Math.abs(change)}}%","") + " \n_____________________________________________\n";
+                    balance = balance.Replace(" ", "").Replace("\n", "").Replace("{{Math.abs(change)}}%", "");
+
+                    if (balance == "0BTC0USD")
+                    {
+                        result = string.Empty;
+                    }
+                    else
+                    {
+                        result = blockch + " Network, Wallet: " + wallet + " | Balance: " + balance + " \n_____________________________________________\n";
+                    }
+
                 }
                 else
                 {
